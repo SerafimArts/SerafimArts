39a2a01d79cc70d77fbfc421a8ddeec57b8bc336
Serafim <nesk@xakep.ru>
2016-06-23 22:46:22 +0300

    Remove analytics middleware for auth

--- app/Interfaces/Http/routes.php
+++ app/Interfaces/Http/routes.php
@@ -4,12 +4,12 @@ Route::group(['middleware' => ['analityc']], function() {
     Route::get('/', 'HomeController@index')->name('home');
 
     Route::get('/article/{url}', 'ArticleController@show')->name('article');
+    
+});
 
-    //
-    Route::group(['prefix' => '/auth'], function () {
-        Route::get('/', 'AuthController@show')->name('login');
-        Route::post('login', 'AuthController@login')->name('login.action');
-        Route::any('logout', 'AuthController@logout')->name('logout');
-    });
-
+//
+Route::group(['prefix' => '/auth'], function () {
+    Route::get('/', 'AuthController@show')->name('login');
+    Route::post('login', 'AuthController@login')->name('login.action');
+    Route::any('logout', 'AuthController@logout')->name('logout');
 });
