e620b410adc35d7064095853c49e2a7f76bc4abb
Serafim <nesk@xakep.ru>
2016-06-29 01:22:22 +0300

    Add youtube lazy load

--- resources/javascripts/app/ViewModels/Article.js
+++ resources/javascripts/app/ViewModels/Article.js
@@ -5,13 +5,28 @@ export default class Article {
     highlightedBlocks = new WeakSet();
 
     /**
+     * @type {WeakMap}
+     */
+    videoBlocks = new WeakMap();
+
+    /**
      * Constructor
      */
     constructor() {
-        var nodes = document.querySelectorAll('pre > code');
-        for (var i = 0; i < nodes.length; i++) {
+        var nodes = [];
+        var i = 0;
+
+        nodes = document.querySelectorAll('pre > code');
+        for (i = 0; i < nodes.length; i++) {
             nodes[i].setAttribute('class', (nodes[i].classList[0] || '').replace('language-', ''));
             nodes[i].setAttribute('data-bind', 'inview: function(state, value) { highlight(state, value); }')
+        }
+
+        nodes = document.querySelectorAll('iframe');
+        for (i = 0; i < nodes.length; i++) {
+            this.videoBlocks.set(nodes[i], nodes[i].getAttribute('src'));
+            nodes[i].setAttribute('src', '');
+            nodes[i].setAttribute('data-bind', 'inview: function(state, value) { loadVideo(state, value); }')
         }
     }
 
@@ -23,6 +38,16 @@ export default class Article {
         if (state && !this.highlightedBlocks.has(node)) {
             this.highlightedBlocks.add(node);
             hljs.highlightBlock(node);
+        }
+    }
+
+    /**
+     * @param {HTMLElement} node
+     * @param {bool} state
+     */
+    loadVideo(node, state) {
+        if (state && !node.getAttribute('src')) {
+            node.setAttribute('src', this.videoBlocks.get(node));
         }
     }
 }
