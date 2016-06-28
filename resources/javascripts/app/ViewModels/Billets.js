12da462aac3c3ca49ff6105d05aa354b1bda26c8
Serafim <nesk@xakep.ru>
2016-06-29 00:58:21 +0300

    Improve parallax

--- resources/javascripts/app/ViewModels/Billets.js
+++ resources/javascripts/app/ViewModels/Billets.js
@@ -9,5 +9,28 @@ export default class Billets {
                 }, i * 300);
             })(i);
         }
+
+        var list = dom.querySelector('[data-id=billets-list]');
+        var nav = dom.querySelector('[data-id=billets-nav]');
+
+        window.addEventListener('scroll', event => {
+            Billets.checkScroll(list, nav);
+        }, true);
+        Billets.checkScroll(list, nav);
+    }
+
+    /**
+     * @param {HTMLElement} list
+     * @param {HTMLElement} nav
+     */
+    static checkScroll(list, nav) {
+        var scrollY = window.pageYOffset - 300;
+
+        if (scrollY > 0) {
+            if (nav.getBoundingClientRect().height + scrollY < nav.parentElement.getBoundingClientRect().height) {
+                list.style.marginTop = `-${scrollY / 2}px`;
+                nav.style.marginTop = `${scrollY}px`;
+            }
+        }
     }
 }
