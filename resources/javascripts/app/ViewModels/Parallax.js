9dbefbd4349bb7f10d9c98890dae18f19cd50453
Serafim <nesk@xakep.ru>
2016-09-04 23:05:22 +0300

    Improve header <3

--- resources/javascripts/app/ViewModels/Parallax.js
+++ resources/javascripts/app/ViewModels/Parallax.js
@@ -45,9 +45,43 @@ export default class Parallax {
     hidden = ko.observable(false);
 
     /**
+     * @type {HTMLVideoElement}
+     */
+    video = null;
+
+    /**
+     * @type {CanvasRenderingContext2D}
+     */
+    ctx = null;
+
+    /**
+     * @type {Image}
+     */
+    bg = null;
+
+    /**
+     * @type {Image}
+     */
+    logo = null;
+
+    /**
+     * @type {number}
+     */
+    logoPosition = 0;
+
+    /**
+     * @type {number}
+     */
+    logoOverlay = 0;
+
+    /**
      * @constructor
      */
     constructor(dom) {
+        this.video = dom.querySelector('[data-id=video]');
+        this.video.play();
+        this.video.loop = true;
+
         this.height(this.clientHeight);
         this.scrollToNode = dom.querySelector('[data-id=scrollTo]');
 
@@ -56,9 +90,18 @@ export default class Parallax {
         canvas.setAttribute('width', this.clientWidth);
         canvas.setAttribute('height', this.clientHeight);
 
+        this.ctx = canvas.getContext('2d');
+
+        this.bg = document.createElement('img');
+        this.bg.src = '/img/parallax/triangles-white.png';
+
+        this.logo = document.createElement('img');
+        this.logo.src = '/img/parallax/logo.png';
+
         this.ribbons = [
             new Ribbon(canvas, this.clientWidth, this.clientHeight),
-            new Ribbon(canvas, this.clientWidth, this.clientHeight)
+            new Ribbon(canvas, this.clientWidth, this.clientHeight),
+            new Ribbon(canvas, this.clientWidth, this.clientHeight),
         ];
 
         this.render();
@@ -75,12 +118,58 @@ export default class Parallax {
     }
 
     render() {
-        this.ribbons[0].ctx.clearRect(0, 0, this.ribbons[0].width, this.ribbons[0].height);
-        this.ribbons[0].ctx.globalAlpha = .9;
-        this.ribbons[0].ctx.globalCompositeOperation = 'lighter';
+        if (this.logoPosition !== 0) {
+            this.logoPosition = 0;
+        } else if (Math.random() > .98) {
+            this.logoPosition = 1;
+        } else if (Math.random() < .02) {
+            this.logoPosition = 2;
+        }
+
+        if (this.logoOverlay !== 0) {
+            this.logoOverlay = 0;
+        } else if (Math.random() > .4) {
+            this.logoOverlay = 1;
+        }
+
+        //this.ribbons[0].ctx.clearRect(0, 0, this.ribbons[0].width, this.ribbons[0].height);
+        this.ctx.drawImage(this.bg, 0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+
+        this.ctx.save();
+        this.ctx.globalAlpha = .9;
         for (var i = 0; i < this.ribbons.length; i++) {
+            if (i > 1) {
+                this.ctx.globalCompositeOperation = 'lighter';
+            }
             this.ribbons[i].render();
         }
+        this.ctx.restore();
+
+        this.ctx.save();
+        this.ctx.globalCompositeOperation = 'lighter';
+        this.ctx.drawImage(this.video, 0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+        this.ctx.restore();
+
+        this.ctx.save();
+        if (this.logoOverlay === 1) {
+            this.ctx.globalAlpha = Math.min(Math.random() + .7, 1);
+        } else {
+            this.ctx.globalCompositeOperation = 'darken';
+        }
+
+        this.ctx.drawImage(
+            this.logo,
+            0,
+            this.logoPosition * 200,
+            800,
+            200,
+            (this.ctx.canvas.width / 2) - 400 + (this.logoPosition > 0 ? (Math.random() * 20 - 10) : 0),
+            (this.ctx.canvas.height / 2) - 100 + (this.logoPosition > 0 ? (Math.random() * 20 - 10) : 0),
+            800,
+            200
+        );
+        this.ctx.restore();
+
         requestAnimationFrame(() => this.render());
     }
 
