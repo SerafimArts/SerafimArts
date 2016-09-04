140e64b67662bef85fd25215c22aae11ef9dd558
Serafim <nesk@xakep.ru>
2016-09-04 23:22:20 +0300

    Improve header №2 <3

--- resources/javascripts/app/ViewModels/Parallax.js
+++ resources/javascripts/app/ViewModels/Parallax.js
@@ -60,21 +60,6 @@ export default class Parallax {
     bg = null;
 
     /**
-     * @type {Image}
-     */
-    logo = null;
-
-    /**
-     * @type {number}
-     */
-    logoPosition = 0;
-
-    /**
-     * @type {number}
-     */
-    logoOverlay = 0;
-
-    /**
      * @constructor
      */
     constructor(dom) {
@@ -93,12 +78,10 @@ export default class Parallax {
         this.ctx = canvas.getContext('2d');
 
         this.bg = document.createElement('img');
-        this.bg.src = '/img/parallax/triangles-white.png';
-
-        this.logo = document.createElement('img');
-        this.logo.src = '/img/parallax/logo.png';
+        this.bg.src = '/img/parallax/bg.png';
 
         this.ribbons = [
+            new Ribbon(canvas, this.clientWidth, this.clientHeight),
             new Ribbon(canvas, this.clientWidth, this.clientHeight),
             new Ribbon(canvas, this.clientWidth, this.clientHeight),
             new Ribbon(canvas, this.clientWidth, this.clientHeight),
@@ -118,20 +101,6 @@ export default class Parallax {
     }
 
     render() {
-        if (this.logoPosition !== 0) {
-            this.logoPosition = 0;
-        } else if (Math.random() > .98) {
-            this.logoPosition = 1;
-        } else if (Math.random() < .02) {
-            this.logoPosition = 2;
-        }
-
-        if (this.logoOverlay !== 0) {
-            this.logoOverlay = 0;
-        } else if (Math.random() > .4) {
-            this.logoOverlay = 1;
-        }
-
         //this.ribbons[0].ctx.clearRect(0, 0, this.ribbons[0].width, this.ribbons[0].height);
         this.ctx.drawImage(this.bg, 0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
 
@@ -148,26 +117,6 @@ export default class Parallax {
         this.ctx.save();
         this.ctx.globalCompositeOperation = 'lighter';
         this.ctx.drawImage(this.video, 0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
-        this.ctx.restore();
-
-        this.ctx.save();
-        if (this.logoOverlay === 1) {
-            this.ctx.globalAlpha = Math.min(Math.random() + .7, 1);
-        } else {
-            this.ctx.globalCompositeOperation = 'darken';
-        }
-
-        this.ctx.drawImage(
-            this.logo,
-            0,
-            this.logoPosition * 200,
-            800,
-            200,
-            (this.ctx.canvas.width / 2) - 400 + (this.logoPosition > 0 ? (Math.random() * 20 - 10) : 0),
-            (this.ctx.canvas.height / 2) - 100 + (this.logoPosition > 0 ? (Math.random() * 20 - 10) : 0),
-            800,
-            200
-        );
         this.ctx.restore();
 
         requestAnimationFrame(() => this.render());
